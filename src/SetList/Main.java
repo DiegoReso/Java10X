b094f1267f4ac5de4329e1645c2823b06fa3004c
Diego Reis <diegoreso@me.com>
2025-04-10 20:55:16 -0300

    Setlist

---
+++ src/SetList/Main.java
@@ -0,0 +1,31 @@
+package SetList;
+
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        //Elementos nao irao se repetir se forem iguals e sem ordenacao por index
+        Set<String> ninjaSet = new HashSet<>();
+        ninjaSet.add("Naruto");
+        ninjaSet.add("Sasuke");
+        ninjaSet.add("Minato");
+        ninjaSet.add("Naruto");
+        ninjaSet.add("Boruto");
+        ninjaSet.add("Gaara");
+        System.out.println(ninjaSet);
+
+        System.out.println("********excluindo Sasuke*********");
+        ninjaSet.remove("Sasuke");
+        System.out.println(ninjaSet);
+        System.out.println("********excluindo Sasuke*********");
+
+        System.out.println("**********Ninja Tree SET************");
+        Set<String> ninjaTreeSet = new TreeSet<>();
+        ninjaTreeSet.addAll(ninjaSet);
+        System.out.println(ninjaTreeSet);
+        System.out.println("**********Ninja Tree SET************");
+    }
+}
