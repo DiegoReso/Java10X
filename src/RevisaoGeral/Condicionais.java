fa9d6d40f4ca76ac301542db4cb662a530cff8ea
Diego Reis <diegoreso@me.com>
2025-04-23 13:14:35 -0300

    practicing java basics

--- src/RevisaoGeral/Condicionais.java
+++ src/RevisaoGeral/Condicionais.java
@@ -1,35 +1,74 @@
 package RevisaoGeral;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Condicionais {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //do-while
+
+        char resp;
+        do {
+            System.out.print("Entre com a temperatura em Celius: ");
+            double C = scanner.nextDouble();
+            double F = 9.0 * C / 5.0 + 32.0;
+            System.out.printf("Equivalente a Fahrenheit: %.1f%n", F);
+            System.out.println("Deseja continuar (s/n)");
+            resp = scanner.next().charAt(0);
+        } while (resp != 'n');
+
+
+        //for
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Posicao: " + i);
+        }
+
+        //while
+        int inteiro = 1;
+        int soma = 0;
+        while (inteiro != 0) {
+
+            System.out.println("Entre com o numero inteiro, para parar o programa digite '0' ");
+            inteiro = scanner.nextInt();
+            soma += inteiro;
+        }
+        System.out.println("A soma eh: " + soma);
+
+
+        //condicao ternaria
+        boolean isEmpty = true;
+        String empty = isEmpty ? " Vazio " : " Nao Vazio ";
+
+        System.out.println(empty);
+
         int x = 5;
 
-        if (x > 0){
+        if (x > 0) {
             System.out.println("Imprimir se x for maior do que 5");
-        }else {
+        } else {
             System.out.println("Imprimir se x for menor do que 5");
         }
 
         //praticando
 
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Entre com os minutos consumidos no plano, lembrando que vc tem 100 minutos por 50 reais, cada minuto adicional sera cobrado 2 reais");
         int minutos = scanner.nextInt();
 
-        if(minutos < 100){
+        if (minutos < 100) {
             System.out.println(" Voce ainda nao consumiu todo seu plano, vc ainda tem " + (100 - minutos) + " " +
                     "minutos restantes no seu plano");
-        }else if(minutos > 100){
+        } else if (minutos > 100) {
             System.out.println("Voce ultrapassou o valor da franquia de 100 minutos  e voce tera que pagar " + (minutos - 100) * 2 + " de valor adicional a sua franquia");
-        }else{
+        } else {
             System.out.println("Seu plano foi consumido 100%, fale mais por 2 reais o minuto!");
         }
 
         //switch case
         int diaDaSemana = 2;
-        switch (diaDaSemana){
+        switch (diaDaSemana) {
             case 1:
                 System.out.println("Dia da semana: domingo");
                 break;
---
+++ src/RevisaoGeral/Debugando.java
@@ -0,0 +1,26 @@
+package RevisaoGeral;
+
+import java.util.Scanner;
+
+public class Debugando {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] alunos = {"Diego", "Cailania", "Roskana", "Charles"};
+
+        double media = verificarMedia(alunos,scan);
+        System.out.println("A media dos alunos foi: " + media);
+
+    }
+
+    static double verificarMedia(String[] alunos, Scanner scan){
+
+        double soma = 0;
+        for (String aluno : alunos){
+            System.out.println("Entre com a nota do aluno " + aluno);
+            double nota = scan.nextDouble();
+            soma += nota;
+        }
+        return soma / alunos.length;
+    }
+}
